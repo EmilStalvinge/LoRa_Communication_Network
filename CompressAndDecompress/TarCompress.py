11829aa9796aebd3fdb6cadeaa36f6d624ae25ab
EmilStalvinge <50867974+EmilStalvinge@users.noreply.github.com>
2021-05-04 08:49:11 +0200

    Add files via upload

---
+++ CompressAndDecompress/TarCompress.py
@@ -0,0 +1,52 @@
+import tarfile
+import os.path
+from tqdm import tqdm 
+
+def compress(tar_file, members):
+    """
+    Adds files (`members`) to a tar_file and compress it
+    """
+    # open file for gzip compressed writing
+    tar = tarfile.open(tar_file, mode="w:gz")
+    # with progress bar
+    # set the progress bar
+    progress = tqdm(members)
+    for member in progress:
+        # add file/folder/link to the tar file (compress)
+        tar.add(member)
+        # set the progress description of the progress bar
+        progress.set_description(f"Compressing {member}")
+    # close the file
+    tar.close()
+def decompress(tar_file, path, members=None):
+    """
+    Extracts `tar_file` and puts the `members` to `path`.
+    If members is None, all members on `tar_file` will be extracted.
+    """
+    tar = tarfile.open(tar_file, mode="r:gz")
+    if members is None:
+        members = tar.getmembers()
+    # with progress bar
+    # set the progress bar
+    progress = tqdm(members)
+    for member in progress:
+        tar.extract(member, path=path)
+        # set the progress description of the progress bar
+        progress.set_description(f"Extracting {member.name}")
+    # or use this
+    # tar.extractall(members=members, path=path)
+    # close the file
+    tar.close()
+
+picture="Ghaith3"
+pictureCompressed=picture+".tar.xz"
+compress(pictureCompressed,["1.png"])
+
+
+'''
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+make_tarfile('PIR Motion Sensor','/home/ghaith/Desktop)
+'''
+
